10edf2971993842d108df403e0280a9709d14fa1
Deepak <ddnith@gmail.com>
2015-06-09 22:33:57 +0530

    Fixed user present not being detected bug.

--- app/src/main/java/in/blogspot/alcory/mobileaddictionmeter/MyReceiver.java
+++ app/src/main/java/in/blogspot/alcory/mobileaddictionmeter/MyReceiver.java
@@ -15,25 +15,21 @@ public class MyReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        Log.d("DEEPAK", "DEEPAK USER 1 - " + intent.getAction());
 
         Intent i = new Intent(context,MyService.class);   // start this sticky service to keep process alive
         context.startService(i);
 
-        if(i.getAction()==Intent.ACTION_USER_PRESENT) {
+        if(Intent.ACTION_USER_PRESENT.equals(intent.getAction())) {
 
             //Toast.makeText(context, "Intent Detected.", Toast.LENGTH_LONG).show();
-            Log.d("DEEPAK", "DEEPAK USER " + intent.getAction());
+            Log.d("DEEPAK", "DEEPAK USER 2 - " + intent.getAction());
 
             SharedPreferences sharedPref = context.getSharedPreferences(MyApp.MY_SHARED_PREF, context.MODE_PRIVATE);
             SharedPreferences.Editor editor = sharedPref.edit();
             Date currentDate = new Date();
             editor.putLong(MyApp.LAST_TIME_USER_ULOCKED_SCREEN, currentDate.getTime());
             editor.apply();
-            Log.d("DEEPAK", "DEEPAK LastUserPresentTime " + currentDate.getTime());
-            Long abc = sharedPref.getLong("LastUserPresentTime", 0L);
-            Date theLastDate = new Date(abc);
-            Log.d("DEEPAK", "DEEPAK LastUserPresentTime removing abc " + abc);
         }
         else if(i.getAction()==Intent.ACTION_DATE_CHANGED){
             MyApp.checkAndSetNewDate(context.getApplicationContext());
--- app/src/main/java/in/blogspot/alcory/mobileaddictionmeter/ScreenReceiver.java
+++ app/src/main/java/in/blogspot/alcory/mobileaddictionmeter/ScreenReceiver.java
@@ -3,7 +3,10 @@ package in.blogspot.alcory.mobileaddictionmeter;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.util.Log;
+
+import java.util.Date;
 
 public class ScreenReceiver extends BroadcastReceiver {
 
@@ -18,7 +21,12 @@ public class ScreenReceiver extends BroadcastReceiver {
 
         } else if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
             // AND DO WHATEVER YOU NEED TO DO HERE
-            Log.d("DEEPAK","DEEPAK ScreenReceiver "+intent.getAction());
+            Log.d("DEEPAK","DEEPAK ScreenReceiver check "+intent.getAction());
+            SharedPreferences sharedPref = context.getSharedPreferences(MyApp.MY_SHARED_PREF, context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPref.edit();
+            Date currentDate = new Date();
+            editor.putLong(MyApp.LAST_TIME_USER_ULOCKED_SCREEN, currentDate.getTime());
+            editor.apply();
         }
     }
 
